862ee9cb468049eb8ec37847ff318b26cb109ab9
Xuuaua <vitor.v1999@gmail.com>
2020-09-07 15:39:31 -0300

    Atividade Completa

--- src/poo/br/edu/unidesc/atividade1/Main.java
+++ src/poo/br/edu/unidesc/atividade1/Main.java
@@ -7,9 +7,9 @@ package poo.br.edu.unidesc.atividade1;
 /*          Escreva um programa em java (void main)
             que vai imprimir os números de de 0 a 100, com uma condição:
 
-            quando o número for  múltiplo de 3  escreve FIZZ
+            quando o número for múltiplo de 3 escreve FIZZ
             quando o número for múltiplo de 5 escreve BUZZ
-            quando o número for múltiplo de  3 e 5 escreve FIZZBUZZ
+            quando o número for múltiplo de 3 e 5 escreve FIZZBUZZ
 
             Exemplo:
             1, 2, FIZZ, 4, BUZZ, 6, 7, 8, FIZZ,......
@@ -21,6 +21,18 @@ package poo.br.edu.unidesc.atividade1;
 public class Main {
 
     public static void main(String[] args) {
-        String
+        for (int i=0; i <= 100; i++) {
+            if (i == 0) {
+                System.out.println(i);
+            } else if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
