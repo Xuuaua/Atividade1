0e9af6c19bd6a70b655ed6728b0d48336d0df0aa
Xuuaua <vitor.v1999@gmail.com>
2020-09-07 15:05:59 -0300

    teste

--- src/poo/br/edu/unidesc/atividade1/Main.java
+++ src/poo/br/edu/unidesc/atividade1/Main.java
@@ -1,8 +1,26 @@
 package poo.br.edu.unidesc.atividade1;
 
+//Atividade: Fizz Buzz - Vale 1 ponto de OAT
+//Professor: Wesley Queiroz
+//Materia: Programação orientada a objetos
+
+/*          Escreva um programa em java (void main)
+            que vai imprimir os números de de 0 a 100, com uma condição:
+
+            quando o número for  múltiplo de 3  escreve FIZZ
+            quando o número for múltiplo de 5 escreve BUZZ
+            quando o número for múltiplo de  3 e 5 escreve FIZZBUZZ
+
+            Exemplo:
+            1, 2, FIZZ, 4, BUZZ, 6, 7, 8, FIZZ,......
+            Usar laço de repetição for
+            Usar If/ Else
+
+            */
+
 public class Main {
 
     public static void main(String[] args) {
-
+        String
     }
 }
